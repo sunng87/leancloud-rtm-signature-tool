4cc1131e52b4ec870920189aad27e7b943696207
Ning Sun <sunng@about.me>
2015-04-29 16:17:12 +0800

    (feat) added command output

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use crypto::mac::Mac;
 use crypto::hmac::Hmac;
 use crypto::sha1::Sha1;
 use rustc_serialize::hex::ToHex;
+use rustc_serialize::json;
 
 fn sign (msg: String, key: String) -> String {
     let msg_bytes = msg.into_bytes();
@@ -43,11 +44,42 @@ struct Signature {
     appid: String,
     clientid: String,
     convid: String,
-    action: String,
     members: String,
     timestamp: i64,
     nonce: String,
     signature: String
+}
+
+impl Signature {
+    fn to_json_cmd(&self, action: String) -> String {
+        let normalized_action = action.to_ascii_lowercase();
+
+        match normalized_action.as_ref() {
+            "open" => {
+                format!("{{\"cmd\": \"session\", \"op\":\"open\", \"appId\": \"{}\", \"peerId\": \"{}\", \"t\": {}, \"n\": \"{}\", \"s\": \"{}\"}}",
+                        self.appid, self.clientid, self.timestamp, self.nonce, self.signature)
+            },
+            "start" => {
+                let member_vec: Vec<&str> = self.members.split(":").collect();
+                format!("{{\"cmd\": \"conv\", \"op\":\"start\", \"appId\": \"{}\", \"peerId\": \"{}\", \"t\": {}, \"n\": \"{}\", \"s\": \"{}\", \"m\": {}}}",
+                        self.appid, self.clientid, self.timestamp, self.nonce, self.signature,
+                        json::encode(&member_vec).unwrap())
+            },
+            "add" => {
+                let member_vec: Vec<&str> = self.members.split(":").collect();
+                format!("{{\"cmd\": \"conv\", \"op\":\"add\", \"appId\": \"{}\", \"peerId\": \"{}\", \"t\": {}, \"n\": \"{}\", \"s\": \"{}\", \"m\": {}, \"cid\": \"{}\"}}",
+                        self.appid, self.clientid, self.timestamp, self.nonce, self.signature,
+                        json::encode(&member_vec).unwrap(), self.convid)
+            },
+            "remove" => {
+                let member_vec: Vec<&str> = self.members.split(":").collect();
+                format!("{{\"cmd\": \"conv\", \"op\":\"remove\", \"appId\": \"{}\", \"peerId\": \"{}\", \"t\": {}, \"n\": \"{}\", \"s\": \"{}\", \"m\": {}, \"cid\": \"{}\"}}",
+                        self.appid, self.clientid, self.timestamp, self.nonce, self.signature,
+                        json::encode(&member_vec).unwrap(), self.convid)
+            },
+            _ => { "".to_string() }
+        }
+    }
 }
 
 fn sort_members(members: String) -> String {
@@ -102,6 +134,7 @@ Options:
   --convid <arg>     Set conversation id.
   --members <arg>    Set members list.
   --masterkey <arg>  Provide master key for signing.
+  --cmd-output       Output as json command.
 ");
 
 fn main() {
@@ -120,11 +153,14 @@ fn main() {
         appid: args.flag_appid,
         clientid: args.flag_clientid,
         convid: args.flag_convid,
-        action: args.arg_action,
         members: args.flag_members,
         timestamp: ts,
         nonce: n,
         signature: sig
     };
-    println!("{:?}", result);
+    if args.flag_cmd_output {
+        println!("{}", result.to_json_cmd(args.arg_action));
+    } else {
+        println!("{:?}", result);
+    }
 }
